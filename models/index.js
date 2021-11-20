62dd85d0594be75dbc682b9201e17c4e5873e539
Phil-Welsh <pwelsh8@gmail.com>
2021-11-19 17:51:13 -0800

    Added controllers, models, and routes dirs

---
+++ models/index.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/gamelib';
+
+const configOptions = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+};
+
+mongoose.connect(connectionString, configOptions)
+  .then(() => console.log('MongoDB successfully connected...'))
+  .catch((err) => console.log(`MongoDB connection error: ${err}`));
+
+module.exports = {
+  Game: require('./Game'),
+};
---
+++ server.js
@@ -0,0 +1,19 @@
+// imports
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
+
+const port = process.env.PORT || 4000;
+const app = express();
+
+// TODO: middleware - CORS
+app.use(cors());
+
+// middleware - JSON parsing
+app.use(express.json());
+
+// middleware - API routes
+app.use('/api/v1/games', routes.games);
+
+// listen
+app.listen(port, () => console.log(`Server is running on port ${port}`));
