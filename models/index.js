e29c735d049dad2da3702c0494a193ad4362f9b3
Phil-Welsh <pwelsh8@gmail.com>
2021-12-01 10:44:14 -0800

    Added db URI

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/backbone';
+const connectionString = process.env.MONGODB_URI || 'mongodb+srv://philwelsh:Scramkids91!@sei.4ya2f.mongodb.net/backbone?retryWrites=true&w=majority';
 
 const configOptions = {
   useNewUrlParser: true,
